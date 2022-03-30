b802a18eebf9c65f548c4bc165d52990215fae2f
AkshayShete Shete (ZA) <AkshayShete@absa.africa>
2022-03-30 16:20:26 +0200

    Branch b updated

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -3,7 +3,7 @@ package JavaPrograms;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-//Armstrong Number
+//Armstrong Numberr
 public class ArmstrongNumber {
 
 	public static void main(String[] args)throws Exception {
